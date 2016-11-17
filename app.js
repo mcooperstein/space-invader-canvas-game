cb14da0e0367038e2217c019ba7692219d079d3b
Marc Cooperstein <mcooperstein@gmail.com>
2016-11-16 17:01:25 -0800

    added enemies

--- app.js
+++ app.js
@@ -12,6 +12,40 @@ var y = 50;
 var dx = 2;
 var dy = 2;*/
 var playerBullets = [];
+var enemies = [];
+
+function Enemy(I) {
+    I = I || {};
+    I.active = true;
+    I.age = Math.floor(Math.random() * 128);
+    I.color = "#A2B";
+    I.x = Math.random() * CANVAS_WIDTH;
+    I.y = 0;
+    I.xVelocity = 0;
+    I.yVelocity = 2;
+    I.width = 32;
+    I.height = 32;
+
+    I.inBounds = function () {
+        return I.x >= 0 && I.x <= CANVAS_WIDTH && I.y >= 0 && I.y <= CANVAS_HEIGHT;
+    };
+
+    I.draw = function () {
+        canvas.fillStyle = this.color;
+        canvas.fillRect(this.x, this.y, this.width, this.height);
+    };
+
+    I.update = function () {
+        I.x += I.xVelocity;
+        I.y += I.yVelocity;
+        I.age++;
+        //randomizing the speed at which the objects move horizontally as they fall
+        I.xVelocity = 3 * Math.sin(I.age * Math.PI / 64);
+
+        I.active = I.active && I.inBounds();
+    };
+    return I;
+};
 
 function Bullet(I) {
     I.active = true;
@@ -90,12 +124,25 @@ function update() {
     playerBullets = playerBullets.filter(function (bullet) {
         return bullet.active;
     });
-}
+
+    enemies.forEach(function (enemy) {
+        enemy.update();
+    });
+    enemies = enemies.filter(function (enemy) {
+        return enemy.active;
+    });
+    if (Math.random() < 0.1) {
+        enemies.push(Enemy());
+    }
+};
 
 function draw() {
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     player.draw();
     playerBullets.forEach(function (bullet) {
         bullet.draw();
+    });
+    enemies.forEach(function (enemy) {
+        enemy.draw();
     });
 }
